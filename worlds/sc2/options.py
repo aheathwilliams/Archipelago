7a7e9ff7e8ee6e482827ff66b125475990a21abb
Alex Williams <a.heath.williams@gmail.com>
2026-02-22 21:11:26 -0600

    sc2: Add option to limit Zerg variants per unit (#2)
    * feat: Add ability to limit max zerg strains per unit
    
    * fix: Register options for Zerg variants

--- worlds/sc2/options.py
+++ worlds/sc2/options.py
@@ -1358,6 +1358,19 @@ class FillerItemsDistribution(ItemDict):
             raise Exception("Cannot have negative item weight.")
         super(ItemDict, self).__init__(value)
 
+class MaxStrainsPerZergUnit(Range):
+    """Maximum number of strains per Zerg unit in the item pool."""
+    display_name = "Max Strains Per Zerg Unit"
+    range_start = 1
+    range_end = 2
+    default = 2
+
+class MaxAspectsPerZergUnit(Range):
+    """Maximum number of aspects per Zerg unit in the item pool."""
+    display_name = "Max Aspects Per Zerg Unit"
+    range_start = 1
+    range_end = 2
+    default = 2
 
 @dataclass
 class Starcraft2Options(PerGameCommonOptions):
@@ -1447,6 +1460,8 @@ class Starcraft2Options(PerGameCommonOptions):
     research_cost_reduction_per_item: ResearchCostReductionPerItem
     filler_items_distribution: FillerItemsDistribution
     mission_order_scouting: MissionOrderScouting
+    max_strains_per_zerg_unit: MaxStrainsPerZergUnit
+    max_aspects_per_zerg_unit: MaxAspectsPerZergUnit
 
     custom_mission_order: CustomMissionOrder
 
